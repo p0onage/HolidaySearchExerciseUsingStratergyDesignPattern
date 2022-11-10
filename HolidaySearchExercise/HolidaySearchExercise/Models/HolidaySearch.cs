3fec34b93691341b46cdb87452b1dac11a6d3363
Joshua Duxbury <joshua.duxbury@tla.co>
2022-11-10 19:59:32 +0000

    Added Holiday Search And Search Results Models

--- HolidaySearchExercise/HolidaySearchExercise/Models/HolidaySearch.cs
+++ HolidaySearchExercise/HolidaySearchExercise/Models/HolidaySearch.cs
@@ -2,5 +2,8 @@
 
 public class HolidaySearch
 {
-    
+    public string DepartingFrom { get; set; }
+    public string TravelingTo { get; set; }
+    public string DepartureDate { get; set; }
+    public string Duration { get; set; }
 }
--- HolidaySearchExercise/HolidaySearchExercise/Models/SearchResults.cs
+++ HolidaySearchExercise/HolidaySearchExercise/Models/SearchResults.cs
@@ -2,5 +2,11 @@
 
 public class SearchResults
 {
-    
+    public SearchResults(List<Hotel> Hotels, List<Flight> Flights)
+    {
+        this.Hotels = Hotels;
+        this.Flights = Flights;
+    }
+    public IEnumerable<Hotel> Hotels { get; set; }
+    public IEnumerable<Flight> Flights { get; set; }
 }
